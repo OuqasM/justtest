3573fe5079f1210125dfdd51228241eba2e82102
unknown <ouqas.pro@gmail.com>
2022-05-04 15:03:16 +0000

    second commit temp branch

--- src/main/java/ma/isga/Test.java
+++ src/main/java/ma/isga/Test.java
@@ -1,6 +1,10 @@
 package ma.isga;
+/*
+comment 
+ */
 public class Test {
     public static void main(String[] args){
+        //nouvelle version 
         System.out.println("hello world");
     }
 }
